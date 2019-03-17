2cf4527693143e86a76a677d8e7b8e04eb0a4a82
Alan Ficagna <alan.ficagna@gmail.com>
2019-03-17 14:59:34 -0300

    Add the rest of the fields in the record.

--- lib/app/record.ex
+++ lib/app/record.ex
@@ -5,13 +5,34 @@ defmodule App.Record do
   use Ecto.Schema
   import Ecto.Changeset
 
+  @fields [:league, :season, :date, :hometeam, :awayteam, :fthg, :ftag, :ftr, :hthg, :htag, :htr]
+
   schema "record" do
     field(:league)
     field(:season)
+    field(:hometeam)
+    field(:awayteam)
+    field(:fthg)
+    field(:ftag)
+    field(:ftr)
+    field(:date)
+    field(:hthg)
+    field(:htag)
+    field(:htr)
   end
 
+  @doc ~S"""
+
+      ## Examples
+
+      iex> App.Record.file_changeset(%{season: "DF1"})
+      %{changes: %{season: "DF1"}}
+
+  """
   def file_changeset(params) do
     %__MODULE__{}
-    |> cast(params, [:league, :season])
+    |> cast(params, fields())
   end
+
+  def fields, do: @fields
 end
