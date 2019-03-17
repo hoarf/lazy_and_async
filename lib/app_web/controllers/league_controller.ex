8c1b0949903b82461e8ffa371a7a83b98dc783b3
Alan Ficagna <alan.ficagna@gmail.com>
2019-03-17 16:13:21 -0300

    Refactor controller
    Extract common code to helper function and change the order, allowing the most
    specific to be matched first.

--- lib/app_web/controllers/league_controller.ex
+++ lib/app_web/controllers/league_controller.ex
@@ -1,19 +1,17 @@
 defmodule AppWeb.LeagueController do
   use AppWeb, :controller
 
-  def index(conn, _params) do
-    with {:ok, records} <- App.League.list() do
-      conn
-      |> put_status(:ok)
-      |> render("index.json", leagues: records)
-    end
+  def index(conn, %{"league" => league, "season" => season}) do
+    index_for(conn, App.League.get_by(league, season))
   end
 
-  def show(conn, %{"league" => league, "season" => season}) do
-    with {:ok, records} <- App.League.get_by(league, season) do
-      conn
-      |> put_status(:ok)
-      |> render("show.json", league: records)
-    end
+  def index(conn, _params) do
+    index_for(conn, App.League.list())
+  end
+
+  defp index_for(conn, {:ok, records}) do
+    conn
+    |> put_status(:ok)
+    |> render("index.json", leagues: records)
   end
 end
