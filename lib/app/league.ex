cbc7dcc3675a53fdd3fd85e369a412328f733f10
Alan Ficagna <alan.ficagna@gmail.com>
2019-03-17 14:50:10 -0300

    Implement the rest of the API.

--- lib/app/league.ex
+++ lib/app/league.ex
@@ -5,7 +5,13 @@ defmodule App.League do
 
   """
 
+  alias App.LeagueCache
+
+  def list() do
+    LeagueCache.list()
+  end
+
   def get_by(league, season) do
-    []
+    LeagueCache.get_by(league, season)
   end
 end
