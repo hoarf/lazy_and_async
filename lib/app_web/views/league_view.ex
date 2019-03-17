e97791048cb4b6b0a2e3539949a25bb5d836aaba
Alan Ficagna <alan.ficagna@gmail.com>
2019-03-17 16:14:29 -0300

    Remove the show method.
    There are only index routes.

--- lib/app_web/views/league_view.ex
+++ lib/app_web/views/league_view.ex
@@ -1,12 +1,8 @@
 defmodule AppWeb.LeagueView do
   use AppWeb, :view
 
-  def render("index.json", %{leagues: leagues}) do
-    render_many(leagues, __MODULE__, "record.json")
-  end
-
-  def render("show.json", %{league: record}) do
-    render_one(record, __MODULE__, "record.json")
+  def render("index.json", %{leagues: records}) do
+    render_many(records, __MODULE__, "record.json")
   end
 
   def render("record.json", %{league: record}) do
