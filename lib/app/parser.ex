932bf41a7853f7f19fe3278f905398875c9e3811
Alan Ficagna <alan.ficagna@gmail.com>
2019-03-17 14:49:25 -0300

    Reorganizes methods and fixes docs.

--- lib/app/parser.ex
+++ lib/app/parser.ex
@@ -1,30 +1,18 @@
 defmodule App.Parser do
-  @moduledoc """
+  @moduledoc ~S"""
   Module responsible for receiving a stream of raw strings and
   converting into structured data.
   """
 
   require Logger
 
-  @doc """
+  @doc ~S"""
   Chunk will partition the stream into records an errors.
 
   """
   def chunk(stream) do
     Stream.chunk_while(stream, %{oks: [], errors: []}, &chunk_fn/2, &after_fn/1)
   end
-
-  def parse({:ok, %{"Div" => league, "Season" => season}}) do
-    case App.Record.file_changeset(%{league: league, season: season}) do
-      %{valid?: true} = changeset ->
-        [ok: Ecto.Changeset.apply_changes(changeset)]
-
-      changeset ->
-        [error: changeset]
-    end
-  end
-
-  def parse(parse_error), do: parse_error
 
   defp chunk_fn(item, acc) do
     case item do
@@ -34,4 +22,21 @@ defmodule App.Parser do
   end
 
   defp after_fn(acc), do: {:cont, acc, []}
+
+  @doc ~S"""
+  Parses a record from the CSV in the internal representation for the system
+  which is %App.Record. It will ignore errors from CSV.
+
+  """
+  def parse({:ok, params}) do
+    case App.Record.file_changeset(params) do
+      %{valid?: true} = changeset ->
+        [ok: Ecto.Changeset.apply_changes(changeset)]
+
+      changeset ->
+        [error: changeset]
+    end
+  end
+
+  def parse(parse_error), do: parse_error
 end
