7f662437c34f38f32447286498e4a7e6bfa95714
Alan Ficagna <alan.ficagna@gmail.com>
2019-03-20 06:28:33 -0300

    Add Halthcheck route.

--- lib/app_web/router.ex
+++ lib/app_web/router.ex
@@ -8,6 +8,8 @@ defmodule AppWeb.Router do
   scope "/", AppWeb do
     pipe_through(:api)
 
+    # This route is used for health check by haproxy.
+    get("/", DefaultController, :index)
     get("/leagues", LeagueController, :index)
     get("/leagues/:league/seasons/:season", LeagueController, :index)
   end
