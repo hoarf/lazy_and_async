92dac499c78441c37331d2c233a2169cac63a21d
Alan Ficagna <alan.ficagna@gmail.com>
2019-03-24 15:26:37 -0300

    Add page info to view.

--- lib/app_web/views/league_view.ex
+++ lib/app_web/views/league_view.ex
@@ -2,7 +2,13 @@ defmodule AppWeb.LeagueView do
   use AppWeb, :view
 
   def render("index.json", %{leagues: records}) do
-    render_many(records, __MODULE__, "record.json")
+    %{
+      entries: render_many(records, __MODULE__, "record.json"),
+      page_number: records.page_number,
+      page_size: records.page_size,
+      total_pages: records.total_pages,
+      total_entries: records.total_entries
+    }
   end
 
   def render("record.json", %{league: record}) do
